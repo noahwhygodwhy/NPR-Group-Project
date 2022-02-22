5519d74ac45f8bed0b0ddb7407ca533c548205d0
noahwhygodwhy <noah.mathnerd.alvard@gmail.com>
2022-02-22 13:44:43 -0500

    implemented basic shader system and made a spinning triangle

--- NPR-Group-Project/NPR-Group-Project.cpp
+++ NPR-Group-Project/NPR-Group-Project.cpp
@@ -2,13 +2,26 @@
 //
 
 #include <iostream>
+#include <vector>
 
 #include "glad/glad.h"
-#include "GLFW/glfw3.h"
-#include "glm/glm.hpp"
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include <glm/gtc/matrix_transform.hpp>`
+#include "Shader.hpp"
 
 using namespace std;
 using namespace glm;
+
+struct Vertex  {
+    vec3 pos;
+    vec3 color;
+    Vertex(vec3 p, vec3 c) {
+        this->pos = p;
+        this->color = c;
+    }
+};
 
 int main()
 {
@@ -36,6 +49,67 @@ int main()
     {
         cout << "GLAD init failed" << endl;
         exit(-1);
+    }
+
+    vector<Vertex> vertices = {
+        Vertex(vec3(0.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0)),
+        Vertex(vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0)),
+        Vertex(vec3(0.0, 1.0, 0.0), vec3(0.0, 0.0, 1.0)),
+    };
+    vector<int> indices = {
+        0, 1, 2
+    };
+
+
+
+    unsigned int VAO, VBO, EBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    // vertex positions
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+    // vertex normals
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+
+    glBindVertexArray(0);
+
+    Shader simpleShader("simple.vert", "simple.frag");
+    while (!glfwWindowShouldClose(window)) {
+        double currentTime = glfwGetTime();
+
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        mat4 projection = glm::perspective(radians(70.0), 1.0, 0.1, 100.0);
+        vec3 eye = vec3(sin(currentTime) * 2.0, 0.0, cos(currentTime) * 2.0);
+        mat4 view = glm::lookAt(eye, vec3(0.0), vec3(0.0, 1.0, 0.0));
+
+        simpleShader.use();
+        simpleShader.setMatFour("projection", projection);
+        simpleShader.setMatFour("view", view);
+        simpleShader.setMatFour("model", mat4(1.0));
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+
+        //processInput(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 }
 
